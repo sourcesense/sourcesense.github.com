82f44b950bd5c728a7541d747f8eff0089ad9ae5
maoo <m.pillitu@sourcesense.com>
2011-01-12 23:29:31 +0100

    fixed layout; projects url are linked to the maven-generated site, if exists, otherwise to the default github page

--- index_files/github.js
+++ index_files/github.js
@@ -1,26 +1,40 @@
 jQuery.githubUser = function(username, callback) {
-  jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
+    jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
 }
- 
+
+jQuery.githubBranches = function(username, repo, callback) {
+    jQuery.getJSON("http://github.com/api/v2/json/" + username + "/" + repo + "/branches?callback=?", callback);
+}
+
 jQuery.fn.loadRepositories = function(username) {
-  this.html("<span>Querying GitHub for repositories...</span>");
- 
-  var target = this; 
-  $.githubUser(username, function(data) {
-    var repos = data.user.repositories;
-    sortByNumberOfWatchers(repos);
- 
-    var list = $('<ul/>');
-    target.empty().append(list);
-    $(repos).each(function() {
-      list.append('<li><a href="'+ this.url +'">' + this.name + '</a></li>');
-//      list.append('<dd>' + this.description + '</dd>');
+    this.html("<span>Querying GitHub for repositories...</span>");
+    var target = this;
+    $.githubUser(username,
+    function(data) {
+        var repos = data.user.repositories;
+        sortByNumberOfWatchers(repos);
+        var list = $('<ul/>');
+        target.empty().append(list);
+        $(repos).each(function() {
+            var hasSite = false;
+            try {
+				//Dirty but effective ;-)
+                $.getJSON('http://sourcesense.github.com/' + this.name + '/index.html?callback=?');
+                hasSite = true;
+            } catch(err) {
+                hasSite = false;
+            }
+            if (hasSite) {
+                list.append('<li><a href="http://sourcesense.github.com/' + this.name + '">' + this.name + '</a></li>');
+            } else {
+                list.append('<li><a href="' + this.url + '">' + this.name + '</a></li>');
+            }
+        });
     });
-  });
- 
-  function sortByNumberOfWatchers(repos) {
-    repos.sort(function(a,b) {
-      return b.watchers - a.watchers;
-    });
-  }
+
+    function sortByNumberOfWatchers(repos) {
+        repos.sort(function(a, b) {
+            return b.watchers - a.watchers;
+        });
+    }
 };
